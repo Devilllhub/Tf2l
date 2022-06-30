60ae9e50edf3550bb477f2fc874274b7cd2b7181
Devilllhub <108457831+Devilllhub@users.noreply.github.com>
2022-06-30 15:25:04 +0530

    Update GenerateStringSession.py

--- GenerateStringSession.py
+++ GenerateStringSession.py
@@ -1,9 +1,9 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-print("""Please go-to my.telegram.org for dtail""")
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+print("""Please go-to my.telegram.org for details""")
+APP_ID = int(input("19972681"))
+API_HASH = input("2f3c727cedf7472a67b3151f5f652268")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
